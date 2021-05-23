b27622a6b1afc876f7a4ae650199c550e341b074
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-05-23 14:09:03 +0700

    Complete basic CRUD API

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,6 +12,19 @@ router.get('/', (req, res) => {
   );
   res.json(users);
 });
+router.post('/', (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+  console.log(req.body);
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: 'Please include name and email' });
+  }
+  users.push(newMember);
+  res.json(users);
+});
 
 router.get('/:id', (req, res) => {
   const found = users.some((user) => user.id === parseInt(req.params.id));
@@ -26,18 +39,25 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  const newMember = {
-    id: uuid.v4(),
-    name: req.body.name,
-    email: req.body.email,
-  };
-  console.log(req.body);
-  if (!newMember.name || !newMember.email) {
-    return res.status(400).json({ msg: 'Please include name and email' });
+router.put('/:id', (req, res) => {
+  const index = users.findIndex((user) => user.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(400).json({ msg: `Can't find anyone` });
   }
-  users.push(newMember);
-  res.json(users);
+  req.body.name ? (users[index].name = req.body.name) : '';
+  req.body.email ? (users[index].email = req.body.email) : '';
+  return res.json({ msg: 'users updated', users });
+});
+
+router.delete('/:id', (req, res) => {
+  const index = users.findIndex(
+    (element) => element.id === parseInt(req.params.id)
+  );
+  if (index === -1) {
+    return res.status(400).json({ msg: `Can't find anyone` });
+  }
+  users.splice(index, 1);
+  res.json({ msg: `User deleted`, users });
 });
 
 module.exports = router;

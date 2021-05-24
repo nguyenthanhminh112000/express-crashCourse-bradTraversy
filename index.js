69fab0c8f059abac839235699e6f46f71e69725d
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-05-24 18:27:01 +0700

    Server-side rendered app with express-handlebar template engine

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
+////////////////////////////////IMPORT AND READ FILE
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger.js');
-const users = require('./Users.js');
 const apiRouter = require('./routes/api/users.js');
-const staticRouter = require('./routes/static/static.js');
-// create a Web Server
+const exphbs = require('express-handlebars');
+const users = require('./Users');
+////////////////////////////////// create a Web Server
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// //set up middleware
+// /////////////////////////////////////SET UP MIDDLEWARE
 // app.use(logger);
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//set up the static resources
-app.use(express.static(path.join(__dirname, 'public')));
-//set up routes
-app.use('/api/users/', apiRouter);
-app.use('/', staticRouter);
 
+//////////////////////////////////////SET UP ROUTES
+// //SET UP FOR STATIC RESOURCES
+// app.use(express.static(path.join(__dirname, 'public')));
+//  SET UP API ROUTER
+app.use('/api/users/', apiRouter);
+// SET ROUTE FOR SEVER-SIDE APP
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Nguyen Thanh Minh', users });
+});
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'Nguyen Thanh Dat' });
+});
 /////////////////////////////App running
 app.listen(PORT, () => {
   console.log('Server running on port 5000');
